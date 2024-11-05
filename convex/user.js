7c3e9b9972db0bcdaf3d2d4870c16fbeaf26b50a
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-06 00:20:15 +0400

    authentication

---
+++ convex/user.js
@@ -0,0 +1,28 @@
+import { v } from "convex/values";
+import { mutation } from "./_generated/server";
+
+export const createUser = mutation({
+  args: {
+    email: v.string(),
+    userName: v.string(),
+    imageURL: v.string(),
+  },
+  handler: async (ctx, args) => {
+    //if user already exists
+    const user = await ctx.db
+      .query("users")
+      .filter((q) => q.eq(q.field("email"), args.email))
+      .collect();
+
+    if (user?.length == 0) {
+      await ctx.db.insert("users", {
+        email: args.email,
+        userName: args.userName,
+        imageURL: args.imageURL,
+      });
+      return "Inserted new user successfully";
+    }
+    return "User already exists";
+    //if not then insert new user
+  },
+});
--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,30 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { useMutation } from "convex/react";
 import Image from "next/image";
+import { api } from "../../convex/_generated/api";
+import { useEffect } from "react";
 
 export default function Home() {
+  const { user } = useUser();
+  const createUser = useMutation(api.user.createUser);
+
+  useEffect(() => {
+    if (user) {
+      CheckUser();
+    }
+  }, [user]);
+
+  const CheckUser = async () => {
+    const result = await createUser({
+      email: user?.primaryEmailAddress?.emailAddress,
+      imageURL: user?.imageUrl,
+      userName: user?.fullName,
+    });
+    console.log(result);
+  };
   return (
     <div>
       <Button>Hello</Button>
