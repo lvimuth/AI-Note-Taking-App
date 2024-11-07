37190f1d47b2bd40f9b63b9cf263751bb0501032
Lakshitha Vimuth <lvimuthfb@gmail.com>
2024-11-07 19:40:14 +0400

    dashboard create

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,7 +1,44 @@
+"use client";
+
+import { useUser } from "@clerk/nextjs";
+import { useQuery } from "convex/react";
 import React from "react";
+import { api } from "../../../convex/_generated/api";
+import Image from "next/image";
+import Link from "next/link";
 
 function Dashboard() {
-  return <div>Dashboard</div>;
+  const { user } = useUser();
+
+  const fileList = useQuery(api.fileStorage.GetUserFiles, {
+    userEmail: user?.primaryEmailAddress.emailAddress,
+  });
+  console.log("fileList", fileList);
+  return (
+    <div>
+      <h2 className="font-medium text-3xl">Workspace</h2>
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 mt-10">
+        {fileList?.length > 0
+          ? fileList?.map((file, index) => (
+              <Link href={"/workspace/" + file.fileID}>
+                <div
+                  key={index}
+                  className="flex p-5 shadow-md rounded-md flex-col items-center justify-center border cursor-pointer hover:scale-105 transition-all"
+                >
+                  <Image src={"/pdf.png"} alt="file" width={50} height={50} />
+                  <h2 className="mt-3 font-medium text-lg">{file?.fileName}</h2>
+                </div>
+              </Link>
+            ))
+          : [1, 2, 3, 4, 5, 6, 7, 8].map((file, index) => (
+              <div
+                key={index}
+                className="bg-gray-100 rounded-md h-[150px] animate-pulse"
+              ></div>
+            ))}
+      </div>
+    </div>
+  );
 }
 
 export default Dashboard;
